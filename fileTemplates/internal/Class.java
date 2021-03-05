26696a91f2369d1f5ad6b62db0f8ee9098e67eb8
jie <jie@DESKTOP-HLMAFM1>
2021-03-05 14:41:06 +0800

    IU-2020.3.1 <jie@DESKTOP-HLMAFM1 Overwrite remote https://github.com/StrivesJie/IdeaSettings.git to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
- * @Auther: wsj
- * @Date: ${DATE} ${HOUR}
- * @Description: 
- */
-public class ${NAME} {
-}
