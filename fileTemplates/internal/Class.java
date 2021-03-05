d802c8ead65f6e2122b256e293df51ec3b525793
jie <jie@DESKTOP-HLMAFM1>
2021-03-05 14:41:07 +0800

    IU-2020.3.1 <jie@DESKTOP-HLMAFM1 Create debugger.xml, updates.xml, ActionScript.xml, rainbow_brackets.xml, keymap.xml, editor.xml, _@user_Material Solarized Light.icls, GSP.xml, projectView.xml, Default.xml, AngularJS .xml, ide.general.xml, project.default.xml, AndroidComments.xml, AndroidLog.xml, JSP.xml, Web Services.xml, shared-indexes.xml, wsl.distributions.xml, FadeComments.xml, databaseSettings.xml, pluginAdvertiser.xml, _@user_Darcula.icls, Zen HTML.xml, laf.xml, ide-features-trainer.xml, Default.xml, RESTful Web Services.xml, JavaScript.xml, _@user_Material Darker.icls, GrepConsole.xml, yiiguxing.translation.xml, AndroidParcelable.xml, jdk.table.xml, lightEdit.xml, Kotlin.xml, notifications.xml, Zen XSL.xml, plain.xml, surround.xml, filetypes.xml, Android.xml, iterations.xml, OGNL.xml, templates.xml, CodeGlance.xml, lombok-plugin.xml, other.xml, colors.scheme.xml, databaseDrivers.xml, _@user_Default.icls, user.xml, Zen CSS.xml, baseRefactoring.xml, xsl.xml, JavaScript Testing.xml, html_xml.xml, markdown.xml, path.macros.xml, ui.lnf.xml, Groovy.xml, SQL.xml, web-types-registry.xml, output.xml, Angular.xml, OGNL _Struts 2_.xml, AndroidXML.xml, Class.java, proxy.settings.xml, Relax Your Eyes.xml, ColdFusion.xml, HTTP Request.xml, mybatis.xml, EasyEyesYellow.icls, find.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Auther: wsj
+ * @Date: ${DATE} ${HOUR}
+ * @Description: 
+ */
+public class ${NAME} {
+}
